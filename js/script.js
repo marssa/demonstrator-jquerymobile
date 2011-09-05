36c0d083c931323503e658348a9cd38b69a66d56
alangrech <alan.grech@marsec-xl.org>
2011-09-05 08:04:27 +0200

    Minor modifications on script.js - editing

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
 
 
-$("#move-forward").live('tap', function(event) {
-	//TODO Replace by web service call
-	alert("Boat moving forward");
-});
-
-$("#move-backward").live('tap', function(event) {
-	//TODO Replace by web service call
-   	alert("Boat moving backward"); 
-});
-
-$("#turn-starboard").live('tap', function(event) {
-	//TODO Replace by web service call
-   	alert("Boat turns on start-board"); 
-});
-
-$("#turn-portside").live('tap', function(event) {
-	//TODO Replace by web service call
-   	alert("Boat turns on port-side"); 
-});
-
 $('#Drive').live('pageshow',function(event){
+
+
+	$("#move-forward").live('tap', function(event) {
+		//TODO Replace by web service call
+		alert("Boat moving forward");
+	});
+
+	$("#move-backward").live('tap', function(event) {
+		//TODO Replace by web service call
+	   	alert("Boat moving backward"); 
+	});
+
+	$("#turn-starboard").live('tap', function(event) {
+		//TODO Replace by web service call
+	   	alert("Boat turns on start-board"); 
+	});
+
+	$("#turn-portside").live('tap', function(event) {
+		//TODO Replace by web service call
+	   	alert("Boat turns on port-side"); 
+	});
+	
 	$(function() {
     $( "#vertical-slider" ).slider({
             orientation: "vertical",
