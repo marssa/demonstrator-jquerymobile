f47a251f06999afc0c3eb667710cecfa16ee2aa3
alangrech <alan.grech@marsec-xl.org>
2011-09-05 10:34:15 +0200

    Rudder angle indicator (rudder.js_ on 'Drive' page from dojox.widget.AnalogGauge Headers set to be always visible whilst footers can be only visible on tapping the screen (modifications index.html) layout.css --> 'thrust-slider' changed to 'thrust-indicator'
    Signed-off-by: alangrech <alan.grech@marsec-xl.org>

--- js/rudder.js
+++ js/rudder.js
@@ -1,5 +1,7 @@
 //rudder gauge
 
+$('#Drive').live('pageshow',function(event){
+
           dojo.require("dojox.widget.AnalogGauge");
             dojo.require("dojox.widget.gauge.AnalogArcIndicator");
             dojo.require("dojox.widget.gauge.AnalogNeedleIndicator");
@@ -7,51 +9,33 @@
             dojo.addOnLoad(function() {
                 var gauge = dijit.byId('rudder-indicator');
                 // Used for a gradient arc indicator below:
-                var fill = {
-                    'type': 'linear',
-                    'x1': 50,
-                    'y1': 50,
-                    'x2': 350,
-                    'y2': 350,
-                    'colors': [{
-                        offset: 0,
-                        color: 'black'
-                    },
-                    {
-                        offset: 0.5,
-                        color: 'black'
-                    },
-                    {
-                        offset: 0.75,
-                        color: 'yellow'
-                    },
-                    {
-                        offset: 1,
-                        color: 'red'
-                    }]
-                };
-                gauge.addIndicator(new dojox.widget.gauge.AnalogArcIndicator({
-                    'value': 200,
-                    'width': 20,
-                    'offset': 150,
-                    'color': fill,
-                    'noChange': true,
-                    'hideValues': true
-                }));
+               
+                
                 gauge.addIndicator(new dojox.widget.gauge.AnalogArcIndicator({
                     'value': 80,
                     'width': 10,
                     'offset': 150,
-                    'color': 'blue',
+                    'color': 'red',
                     'title': 'Arc',
                     'hover': 'Arc: 80'
                 }));
+                
+                gauge.addIndicator(new dojox.widget.gauge.AnalogArcIndicator({
+                    'value': 40,
+                    'width': 10,
+                    'offset': 150,
+                    'color': 'green',
+                    'title': 'Arc',
+                    'hover': 'Arc: 80'
+                }));
+                
                 gauge.addIndicator(new dojox.widget.gauge.AnalogNeedleIndicator({
-                    'value': 100,
+                    'value': 0,
                     'width': 8,
                     'length': 150,
-                    'color': 'red',
+                    'color': 'white',
                     'title': 'Needle',
                     'hover': 'Needle: 100'
                 }));
             });
+});
