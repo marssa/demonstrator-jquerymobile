d7dde37c071c9b6bd0c91e8bbbf6e6ddbf2e40aa
alangrech <alan.grech@marsec-xl.org>
2011-10-13 17:13:28 +0200

    Formatting

--- js/script.js
+++ js/script.js
@@ -1,24 +1,13 @@
 $('#Drive').live('pagebeforeshow',function(event){
 	drive();
-	
-	setInterval(function() {
-		$.ajax({
-			async: true,
-			url: "motionControlPage/rudderAndSpeed",
-			type: "GET",
-			dataType: "json",
-			success: function(result){
-				$('#percent-thrust').val(result['motor']['value']);	
-				$('#rudder-angle').val(result['rudder']['value']);	
-			}
-		});
-	}, 500);
+	currentSpeed();
 });
 
 $('#Waypoints').live('pagebeforeshow',function(event){
-	
+	//TODO
 });
 	
+//common functions
 function showHighLight(elementID) {
 	document.getElementById(elementID).style.display = "block";	
 } 
