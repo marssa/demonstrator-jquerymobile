9719ceba57d63eb0c69b7b42f9ea4776c1f1d9c7
alangrech <alan.grech@marsec-xl.org>
2012-07-04 13:07:39 +0200

    deleted navigation.js edited pathPlanner button enable/disable

--- js/googlemap.js
+++ js/googlemap.js
@@ -1,69 +1,19 @@
-var coordinatesInterrupt;
-var polyLine;
-var tmpPolyLine;
-var markers = [];
-var vmarkers = [];
-var arrayPath =[];
-var map = null;
-var boatMarker;
-var contentString = '<div data-role="fieldcontain" id="marker-form" style=" width: 200px; height: 75px"> '
-		+ '<label for="name">Enter  Waypoint Name:</label><br/><br/>'
-		+ '<input type="text" id="textbox" name="wp-name" value=""  />'
-		+ '</div>';
-
-var id = 0;
-var waypoints = {waypoints: []};
-
-$('#NavDisplay').live('pageshow', function() {
-
-	initMap('map_canvas');
-	
-
-
-	boatMarker = new google.maps.Marker({
-		map : map,
-	});
-//	trackPosition();
-//	coordinatesInterrupt = setInterval("trackPosition()", 10000);
-	initPolyline();
-
-	// disabling control buttons
-	$('#reverse_button').addClass('ui-disabled');
-	$('#start_following_button').addClass('ui-disabled');
-	$('#stop_following_button').addClass('ui-disabled');
-
-});
-
-$('#NavDisplay').live('pagehide', function() {
-	clearInterval(coordinatesInterrupt);
-	coordinatesInterrupt = null;
-});
-
-function trackPosition() {
-
-	$.ajax({
-		url : "gps/coordinates",
-		dataType : "json",
-		data : {},
-		async : false,
-		success : function(coordinate) {
-			lt = 35.889446; 
-			lg = 14.518411;
-			map.setCenter(new google.maps.LatLng(lt, lg));
-	//		boatMarker.setPosition(new google.maps.LatLng(lt, lg));
-		
-		},
-		error : function(result) {
-			clearInterval(coordinatesInterrupt);
-			alert(result.status);
-		}
-	});
-
-};
+var mapObjects = {
+	polyLine : null,
+	tmpPolyLine : null,
+	markers : [],
+	vmarkers : [],
+	arrayPath : [],
+	map : null,
+	id : 0,
+	waypoints : {
+		waypoints : []
+	}
+}
 
 function initMap(mapHolder) {
-	markers = [];
-	vmarkers = [];
+	mapObjects.markers = [];
+	mapObjects.vmarkers = [];
 	var mapOptions = {
 		zoom : 25,
 		center : new google.maps.LatLng(35.88923, 14.51721),
@@ -73,22 +23,193 @@ function initMap(mapHolder) {
 		disableDoubleClickZoom : true
 	};
 
-	map = new google.maps.Map(document.getElementById(mapHolder), mapOptions);
-	google.maps.event.addListener(map, "click", mapLeftClick);
+	mapObjects.map = new google.maps.Map(document.getElementById(mapHolder),
+			mapOptions);
+	google.maps.event.addListener(mapObjects.map, "click", mapLeftClick);
 	mapHolder = null;
 	mapOptions = null;
 
 };
-$('#NavDisplay').live('pageshow', function() {
 
-$("#new_trip_button").live('vmousedown', function() {
-	initMap('map_canvas');
-	initPolyline();
-	waypoints = {waypoints: []};
-	markers = [];
+$('#NavDisplay').live(
+		'pageshow',
+		function() {
+			
+			var coordinatesInterrupt;
+			initMap('map_canvas');
 
-});
-});
+			boatMarker = new google.maps.Marker({
+				map : mapObjects.map,
+			});
+			// trackPosition();
+			// coordinatesInterrupt = setInterval("trackPosition()", 10000);
+			initPolyline();
+
+			// disabling control buttons
+			$('#reverse_button').addClass('ui-disabled');
+			$('#start_following_button').addClass('ui-disabled');
+			$('#stop_following_button').addClass('ui-disabled');
+
+			$('#NavDisplay').live('pagehide', function() {
+				clearInterval(coordinatesInterrupt);
+				coordinatesInterrupt = null;
+			});
+
+			$("#new_trip_button").live('vmousedown', function() {
+				mapObjects.waypoints = {
+					waypoints : []
+				};
+				initMap('map_canvas');
+				initPolyline();
+				mapObjects.markers = [];
+			});
+
+			$('#reverse_button').click(function() {
+
+				$.ajax({
+					type : "POST",
+					contentType : "application/json; charset=utf-8",
+					url : "pathPlanner/reverseRoute",
+					data : "{}",
+					dataType : "json",
+					success : function() {
+						alert("Reversing Route!");
+					},
+					error : function() {
+						alert("Reverse Route not working!");
+					}
+				});
+
+			});
+
+			$('#start_following_button').click(function() {
+
+				$.ajax({
+					type : "POST",
+					contentType : "application/json; charset=utf-8",
+					url : "pathPlanner/startFollowing",
+					data : "{}",
+					dataType : "json",
+					success : function() {
+						alert("Started Following!");
+						$('#stop_following_button').removeClass(
+						'ui-disabled');
+						$('#start_following_button').addClass(
+						'ui-disabled');
+					},
+					error : function() {
+						alert("Start Following - not working!");
+					}
+				});
+
+			});
+
+			$('#stop_following_button').click(function() {
+
+				$.ajax({
+					type : "POST",
+					contentType : "application/json; charset=utf-8",
+					url : "pathPlanner/stopFollowing",
+					data : "{}",
+					dataType : "json",
+					success : function() {
+						alert("Stopped Following!");
+						
+						$('#reverse_button').removeClass('ui-disabled');
+				$('#start_following_button').removeClass(
+						'ui-disabled');
+				$('#stop_following_button').removeClass(
+						'ui-disabled');
+					},
+					error : function() {
+						alert("Stop Following - not working!");
+					}
+				});
+
+			});
+
+			$('#come_home_button').click(function() {
+
+				$.ajax({
+					type : "POST",
+					contentType : "/application/json; charset=utf-8",
+					url : "pathPlanner/comeHome",
+					data : "{}",
+					dataType : "json",
+					success : function() {
+						alert("Coming Home!");
+					},
+					error : function() {
+						alert("Coming Home - not working!");
+					}
+				});
+
+			});
+
+			$('#pass_waypoints_button').click(
+					function() {
+						$.ajax({
+							type : "POST",
+							contentType : "application/json; charset=utf-8",
+							url : "pathPlanner/waypoints",
+							data : JSON.stringify(mapObjects.waypoints),
+							dataType : "json",
+							statusCode : {
+								200 : function() {
+									alert("No Waypoints selected!");
+								},
+								201 : function() {
+
+									alert("Waypoints sent successfully!"
+											+ JSON.stringify(mapObjects.waypoints));
+									$('#reverse_button').removeClass(
+											'ui-disabled');
+									$('#start_following_button').removeClass(
+											'ui-disabled');
+									$('#pass_waypoints_button').addClass(
+									'ui-disabled');
+							$('#new_trip_button').addClass(
+									'ui-disabled');
+								},
+								404 : function() {
+									alert("Unsuccesful:"
+											+ JSON.stringify(mapObjects.waypoints));
+
+								},
+								500 : function() {
+									alert("An Internal Error:"
+											+ JSON.stringify(mapObjects.waypoints));
+
+								}
+							}
+
+						});
+
+					});
+			
+			function trackPosition() {
+
+				$.ajax({
+					url : "gps/coordinates",
+					dataType : "json",
+					data : {},
+					async : false,
+					success : function(coordinate) {
+						lt = 35.889446;
+						lg = 14.518411;
+						mapObjects.map.setCenter(new google.maps.LatLng(lt, lg));
+						// boatMarker.setPosition(new google.maps.LatLng(lt, lg));
+
+					},
+					error : function(result) {
+						clearInterval(coordinatesInterrupt);
+						alert(result.status);
+					}
+				});
+
+			};
+
+		});
 
 var initPolyline = function() {
 	var polyOptions = {
@@ -101,34 +222,33 @@ var initPolyline = function() {
 		strokeOpacity : 0.4,
 		strokeWeight : 4
 	};
-	polyLine = new google.maps.Polyline(polyOptions);
-	polyLine.setMap(map);
-	tmpPolyLine = new google.maps.Polyline(tmpPolyOptions);
-	tmpPolyLine.setMap(map);
+	mapObjects.polyLine = new google.maps.Polyline(polyOptions);
+	mapObjects.polyLine.setMap(mapObjects.map);
+	mapObjects.tmpPolyLine = new google.maps.Polyline(tmpPolyOptions);
+	mapObjects.tmpPolyLine.setMap(mapObjects.map);
 };
-
 
 var mapLeftClick = function(event) {
 
-	var path = polyLine.getPath();
+	var path = mapObjects.polyLine.getPath();
 	if (event.latLng) {
-	//Code used when tracking position is on
-		/*	if (markers.length ==0){
-			var firstMarker = createMarker(boatMarker.getPosition());
-			markerName= 'Boat Start Position';
-			markers.push(firstMarker);
-			
-			path.push(boatMarker.getPosition());
-		}*/
+		// Code used when tracking position is on
+		/*
+		 * if (markers.length ==0){ var firstMarker =
+		 * createMarker(boatMarker.getPosition()); markerName= 'Boat Start
+		 * Position'; markers.push(firstMarker);
+		 * 
+		 * path.push(boatMarker.getPosition()); }
+		 */
 		var marker = createMarker(event.latLng);
-		markers.push(marker);
-				
-		if (markers.length != 1) {
+		mapObjects.markers.push(marker);
+
+		if (mapObjects.markers.length != 1) {
 			var vmarker = createVMarker(event.latLng);
-			vmarkers.push(vmarker);
+			mapObjects.vmarkers.push(vmarker);
 			vmarker = null;
 		}
-			
+
 		path.push(event.latLng);
 		readPolyLine();
 		marker = null;
@@ -137,7 +257,7 @@ var mapLeftClick = function(event) {
 };
 
 var createMarker = function(point) {
-	
+
 	var imageNormal = new google.maps.MarkerImage("img/boat-icon-normal.png",
 			new google.maps.Size(11, 11), new google.maps.Point(0, 0),
 			new google.maps.Point(6, 6));
@@ -146,7 +266,7 @@ var createMarker = function(point) {
 			new google.maps.Point(6, 6));
 	var marker = new google.maps.Marker({
 		position : point,
-		map : map,
+		map : mapObjects.map,
 		icon : imageNormal,
 		draggable : true
 	});
@@ -160,9 +280,9 @@ var createMarker = function(point) {
 	});
 
 	google.maps.event.addListener(marker, "drag", function() {
-		for ( var m = 0; m < markers.length; m++) {
-			if (markers[m] == marker) {
-				polyLine.getPath().setAt(m, marker.getPosition());
+		for ( var m = 0; m < mapObjects.markers.length; m++) {
+			if (mapObjects.markers[m] == marker) {
+				mapObjects.polyLine.getPath().setAt(m, marker.getPosition());
 				moveVMarker(m);
 				break;
 			}
@@ -172,11 +292,11 @@ var createMarker = function(point) {
 
 	// removing marker on double click
 	google.maps.event.addListener(marker, "dblclick", function() {
-		for ( var m = 0; m < markers.length; m++) {
-			if (markers[m] == marker) {
+		for ( var m = 0; m < mapObjects.markers.length; m++) {
+			if (mapObjects.markers[m] == marker) {
 				marker.setMap(null);
-				markers.splice(m, 1);
-				polyLine.getPath().removeAt(m);
+				mapObjects.markers.splice(m, 1);
+				mapObjects.polyLine.getPath().removeAt(m);
 				removeVMarkers(m);
 				break;
 			}
@@ -187,7 +307,8 @@ var createMarker = function(point) {
 };
 
 var createVMarker = function(point) {
-	var prevpoint = markers[markers.length - 2].getPosition();
+	var prevpoint = mapObjects.markers[mapObjects.markers.length - 2]
+			.getPosition();
 	var imageNormal = new google.maps.MarkerImage("img/x.png",
 			new google.maps.Size(11, 11), new google.maps.Point(0, 0),
 			new google.maps.Point(6, 6));
@@ -198,7 +319,7 @@ var createVMarker = function(point) {
 		position : new google.maps.LatLng(point.lat()
 				- (0.5 * (point.lat() - prevpoint.lat())), point.lng()
 				- (0.5 * (point.lng() - prevpoint.lng()))),
-		map : map,
+		map : mapObjects.map,
 		icon : imageNormal,
 		draggable : true
 	});
@@ -209,37 +330,39 @@ var createVMarker = function(point) {
 		marker.setIcon(imageNormal);
 	});
 	google.maps.event.addListener(marker, "dragstart", function() {
-		for ( var m = 0; m < vmarkers.length; m++) {
-			if (vmarkers[m] == marker) {
-				var tmpPath = tmpPolyLine.getPath();
-				tmpPath.push(markers[m].getPosition());
-				tmpPath.push(vmarkers[m].getPosition());
-				tmpPath.push(markers[m + 1].getPosition());
+		for ( var m = 0; m < mapObjects.vmarkers.length; m++) {
+			if (mapObjects.vmarkers[m] == marker) {
+				var tmpPath = mapObjects.tmpPolyLine.getPath();
+				tmpPath.push(mapObjects.markers[m].getPosition());
+				tmpPath.push(mapObjects.vmarkers[m].getPosition());
+				tmpPath.push(mapObjects.markers[m + 1].getPosition());
 				break;
 			}
 		}
 		m = null;
 	});
-	google.maps.event.addListener(marker, "drag", function() {
-		for ( var m = 0; m < vmarkers.length; m++) {
-			if (vmarkers[m] == marker) {
-				tmpPolyLine.getPath().setAt(1, marker.getPosition());
-				readPolyLine();
-				break;
-			}
-		}
-		m = null;
-	});
+	google.maps.event.addListener(marker, "drag",
+			function() {
+				for ( var m = 0; m < mapObjects.vmarkers.length; m++) {
+					if (mapObjects.vmarkers[m] == marker) {
+						mapObjects.tmpPolyLine.getPath().setAt(1,
+								marker.getPosition());
+						readPolyLine();
+						break;
+					}
+				}
+				m = null;
+			});
 	google.maps.event.addListener(marker, "dragend", function() {
-		for ( var m = 0; m < vmarkers.length; m++) {
-			if (vmarkers[m] == marker) {
+		for ( var m = 0; m < mapObjects.vmarkers.length; m++) {
+			if (mapObjects.vmarkers[m] == marker) {
 				var newpos = marker.getPosition();
-				var startMarkerPos = markers[m].getPosition();
+				var startMarkerPos = mapObjects.markers[m].getPosition();
 				var firstVPos = new google.maps.LatLng(newpos.lat()
 						- (0.5 * (newpos.lat() - startMarkerPos.lat())), newpos
 						.lng()
 						- (0.5 * (newpos.lng() - startMarkerPos.lng())));
-				var endMarkerPos = markers[m + 1].getPosition();
+				var endMarkerPos = mapObjects.markers[m + 1].getPosition();
 				var secondVPos = new google.maps.LatLng(newpos.lat()
 						- (0.5 * (newpos.lat() - endMarkerPos.lat())), newpos
 						.lng()
@@ -248,13 +371,13 @@ var createVMarker = function(point) {
 				newVMarker.setPosition(secondVPos);// apply the correct
 				// position to the vmarker
 				var newMarker = createMarker(newpos);
-				markers.splice(m + 1, 0, newMarker);
-				polyLine.getPath().insertAt(m + 1, newpos);
+				mapObjects.markers.splice(m + 1, 0, newMarker);
+				mapObjects.polyLine.getPath().insertAt(m + 1, newpos);
 				marker.setPosition(firstVPos);
-				vmarkers.splice(m + 1, 0, newVMarker);
-				tmpPolyLine.getPath().removeAt(2);
-				tmpPolyLine.getPath().removeAt(1);
-				tmpPolyLine.getPath().removeAt(0);
+				mapObjects.vmarkers.splice(m + 1, 0, newVMarker);
+				mapObjects.tmpPolyLine.getPath().removeAt(2);
+				mapObjects.tmpPolyLine.getPath().removeAt(1);
+				mapObjects.tmpPolyLine.getPath().removeAt(0);
 				readPolyLine();
 				newpos = null;
 				startMarkerPos = null;
@@ -272,32 +395,40 @@ var createVMarker = function(point) {
 	return marker;
 };
 
-function readPolyLine(){
-	arrayPath = polyLine.getPath();
-	waypoints = {waypoints: []};
-	id = 0;
-	for (var i=0; i< arrayPath.length;i++){
-		
-		markerName = 'Waypoint #' + id++;
-		waypoint = {name : markerName, id: id, lat : arrayPath.getAt(i).lat(), lng : arrayPath.getAt(i).lng() }
-		waypoints.waypoints.push(waypoint);
+function readPolyLine() {
+	mapObjects.arrayPath = mapObjects.polyLine.getPath();
+	mapObjects.waypoints = {
+		waypoints : []
+	};
+	mapObjects.id = 0;
+	for ( var i = 0; i < mapObjects.arrayPath.length; i++) {
+
+		markerName = 'Waypoint #' + mapObjects.id++;
+		waypoint = {
+			name : markerName,
+			id : mapObjects.id,
+			lat : mapObjects.arrayPath.getAt(i).lat(),
+			lng : mapObjects.arrayPath.getAt(i).lng()
+		}
+		mapObjects.waypoints.waypoints.push(waypoint);
 	}
-	
-	
+
 };
 
 var moveVMarker = function(index) {
-	var newpos = markers[index].getPosition();
+	var newpos = mapObjects.markers[index].getPosition();
 	if (index != 0) {
-		var prevpos = markers[index - 1].getPosition();
-		vmarkers[index - 1].setPosition(new google.maps.LatLng(newpos.lat()
-				- (0.5 * (newpos.lat() - prevpos.lat())), newpos.lng()
-				- (0.5 * (newpos.lng() - prevpos.lng()))));
+		var prevpos = mapObjects.markers[index - 1].getPosition();
+		mapObjects.vmarkers[index - 1].setPosition(new google.maps.LatLng(
+				newpos.lat() - (0.5 * (newpos.lat() - prevpos.lat())), newpos
+						.lng()
+						- (0.5 * (newpos.lng() - prevpos.lng()))));
 		prevpos = null;
 	}
-	if (index != markers.length - 1) {
-		var nextpos = markers[index + 1].getPosition();
-		vmarkers[index].setPosition(new google.maps.LatLng(newpos.lat()
+	if (index != mapObjects.markers.length - 1) {
+		var nextpos = mapObjects.markers[index + 1].getPosition();
+		mapObjects.vmarkers[index].setPosition(new google.maps.LatLng(newpos
+				.lat()
 				- (0.5 * (newpos.lat() - nextpos.lat())), newpos.lng()
 				- (0.5 * (newpos.lng() - nextpos.lng()))));
 		nextpos = null;
@@ -307,21 +438,23 @@ var moveVMarker = function(index) {
 };
 
 var removeVMarkers = function(index) {
-	if (markers.length > 0) {// clicked marker has already been deleted
-		if (index != markers.length) {
-			vmarkers[index].setMap(null);
-			vmarkers.splice(index, 1);
+	if (mapObjects.markers.length > 0) {// clicked marker has already been
+										// deleted
+		if (index != mapObjects.markers.length) {
+			mapObjects.vmarkers[index].setMap(null);
+			mapObjects.vmarkers.splice(index, 1);
 		} else {
-			vmarkers[index - 1].setMap(null);
-			vmarkers.splice(index - 1, 1);
+			mapObjects.vmarkers[index - 1].setMap(null);
+			mapObjects.vmarkers.splice(index - 1, 1);
 		}
 	}
-	if (index != 0 && index != markers.length) {
-		var prevpos = markers[index - 1].getPosition();
-		var newpos = markers[index].getPosition();
-		vmarkers[index - 1].setPosition(new google.maps.LatLng(newpos.lat()
-				- (0.5 * (newpos.lat() - prevpos.lat())), newpos.lng()
-				- (0.5 * (newpos.lng() - prevpos.lng()))));
+	if (index != 0 && index != mapObjects.markers.length) {
+		var prevpos = mapObjects.markers[index - 1].getPosition();
+		var newpos = mapObjects.markers[index].getPosition();
+		mapObjects.vmarkers[index - 1].setPosition(new google.maps.LatLng(
+				newpos.lat() - (0.5 * (newpos.lat() - prevpos.lat())), newpos
+						.lng()
+						- (0.5 * (newpos.lng() - prevpos.lng()))));
 		prevpos = null;
 		newpos = null;
 	}
